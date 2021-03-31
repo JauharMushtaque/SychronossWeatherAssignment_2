36ab38d8ce6a3ec54f892273853e42deac87ef11
jauhar <jauhar009@gmail.com>
2021-03-31 15:55:56 +0530

    Adding Utility methods.

---
+++ app/src/main/java/com/example/synchronoss/test/utils/Utility.kt
@@ -0,0 +1,19 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.synchronoss.test.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
+object  Utility {
+    const val BASE_URL = "https://api.openweathermap.org/"
+
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+        return if (connectivityManager is ConnectivityManager) {
+            val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+            networkInfo?.isConnected ?: false
+        } else false
+    }
+}
